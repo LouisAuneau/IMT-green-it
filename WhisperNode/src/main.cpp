d444ec6cdc07b14bd5fdacbb6bd9789698c7b043
Louis AUNEAU <louis.auneau@amadeus.com>
2018-10-04 16:13:28 +0200

    Basic I/O code.

--- WhisperNode/src/main.cpp
+++ WhisperNode/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
 
+const int LED_ORANGE = 9;
+const int LED_BLUE = 6;
+const int BUTTON_LEFT = 5;
+const int BUTTON_RIGHT = 4;
+
 void setup() {
-    // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.print("Hello World.");
+
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_ORANGE, OUTPUT);
+    pinMode(BUTTON_LEFT, INPUT);
+    pinMode(BUTTON_RIGHT, INPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    if (digitalRead(BUTTON_RIGHT) == HIGH) {
+        digitalWrite(LED_ORANGE, HIGH);
+    } else {
+        digitalWrite(LED_ORANGE, LOW);
+    }
+
+    if (digitalRead(BUTTON_LEFT) == HIGH) {
+        digitalWrite(LED_BLUE, HIGH);
+    } else {
+        digitalWrite(LED_BLUE, LOW);
+    }
 }
