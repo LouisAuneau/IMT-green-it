abb149db203e0b9be93792096b8dada6340420ac
mathieu guyot <mathieuguyot40@yahoo.fr>
2018-10-25 11:39:42 +0200

    Add lib

--- WhisperNode/src/main.cpp
+++ WhisperNode/src/main.cpp
@@ -1,26 +1,27 @@
 #include <Arduino.h>
-#include <Button.h>
+#include "RH_RF95.h"
+#include "T2WhisperNode.h"
+#include "hal/T2Flash.h"
+#include <LowPower.h>
 
-const int LED_ORANGE = 9;
-const int LED_BLUE = 6;
-const int PIN_BUTTON_LEFT = 5;
-const int PIN_BUTTON_RIGHT = 4;
-const Button* BUTTON_LEFT;
-const Button* BUTTON_RIGHT;
+RH_RF95 radio;
+T2Flash flash;
 
 void setup() {
+    //Init SP
     Serial.begin(9600);
-    Serial.print("Hello World.");
-    pinMode(LED_BLUE, OUTPUT);
-    pinMode(LED_ORANGE, OUTPUT);
-    BUTTON_LEFT = new Button(PIN_BUTTON_LEFT);
-    BUTTON_RIGHT = new Button(PIN_BUTTON_RIGHT);
+    
+    //Init & disable radio (for now)
+    radio.init();
+    radio.sleep();
+
+    // Turn off flash (not used)
+    flash.init(T2_WPN_FLASH_SPI_CS);
+    flash.powerDown();
 }
 
 void loop() {
-    BUTTON_LEFT->check();
-    BUTTON_LEFT->getNumber();
-
-    // BUTTON_RIGHT->check();
-    // Serial.print(BUTTON_RIGHT->getNumber());
+    Serial.println("HIBERNATE");
+    delay(1000);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 }
