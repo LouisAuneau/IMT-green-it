ad5efd654af6b09058cc9c8d87b12cf3ddf71314
Louis AUNEAU <louis.auneau@amadeus.com>
2018-10-25 09:37:20 +0200

    Added main.cpp.

--- WhisperNode/src/main.cpp
+++ WhisperNode/src/main.cpp
@@ -1,30 +1,26 @@
 #include <Arduino.h>
+#include <Button.h>
 
 const int LED_ORANGE = 9;
 const int LED_BLUE = 6;
-const int BUTTON_LEFT = 5;
-const int BUTTON_RIGHT = 4;
+const int PIN_BUTTON_LEFT = 5;
+const int PIN_BUTTON_RIGHT = 4;
+const Button* BUTTON_LEFT;
+const Button* BUTTON_RIGHT;
 
 void setup() {
     Serial.begin(9600);
     Serial.print("Hello World.");
-
     pinMode(LED_BLUE, OUTPUT);
     pinMode(LED_ORANGE, OUTPUT);
-    pinMode(BUTTON_LEFT, INPUT);
-    pinMode(BUTTON_RIGHT, INPUT);
+    BUTTON_LEFT = new Button(PIN_BUTTON_LEFT);
+    BUTTON_RIGHT = new Button(PIN_BUTTON_RIGHT);
 }
 
 void loop() {
-    if (digitalRead(BUTTON_RIGHT) == HIGH) {
-        digitalWrite(LED_ORANGE, HIGH);
-    } else {
-        digitalWrite(LED_ORANGE, LOW);
-    }
+    BUTTON_LEFT->check();
+    BUTTON_LEFT->getNumber();
 
-    if (digitalRead(BUTTON_LEFT) == HIGH) {
-        digitalWrite(LED_BLUE, HIGH);
-    } else {
-        digitalWrite(LED_BLUE, LOW);
-    }
+    // BUTTON_RIGHT->check();
+    // Serial.print(BUTTON_RIGHT->getNumber());
 }
